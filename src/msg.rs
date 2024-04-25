d1c4e0be6d5a84a05f1ef76a94abb8b71e094e72
George Ornbo <george@shapeshed.com>
2024-04-25 07:41:49 +0100

    Fix clippy warnings

--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,4 @@
-use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, CosmosMsg};
-use cw_vault_standard::{VaultStandardExecuteMsg, VaultStandardQueryMsg};
 
 use osmosis_std::types::osmosis::tokenfactory::v1beta1::MsgCreateDenom;
 
